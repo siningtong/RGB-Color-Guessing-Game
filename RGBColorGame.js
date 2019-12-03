7ee07a4d820a69f26176f916d5235d2d848e7a25
siningtong <siningtong@gmail.com>
2019-12-03 17:41:14 +0000

    refactor code

--- RGBColorGame.js
+++ RGBColorGame.js
@@ -1,60 +1,54 @@
-let numberOfSquares = 6
-let colors = randomColors(numberOfSquares)
-let pickedColor = pickColor();
+let numberOfSquares = 6;
+let colors = [];
+let pickedColor;
 const colorDisplay = document.getElementById('colorDisplay');
-const messageDisplay = document.querySelector('#message')
-colorDisplay.textContent = pickedColor;
+const messageDisplay = document.querySelector('#message');
+// colorDisplay.textContent = pickedColor;
 const squares = document.querySelectorAll('.square')
 const h1 = document.querySelector('h1')
 const resetButton = document.querySelector('#reset')
 resetButton.addEventListener('click', resetGame)
-//level button selected
-const easyBtn = document.querySelector('#easyBtn')
-const hardBtn = document.querySelector('#hardBtn')
-easyBtn.addEventListener('click', function () {
-  className(easyBtn, hardBtn)
-  numberOfSquares = 3
-  colors = randomColors(numberOfSquares)
-  pickedColor = pickColor()
-  h1.style.background = 'steelblue'
-  messageDisplay.textContent = ''
+const modeButton = document.querySelectorAll('.mode')
+initialGame()
+
+
+function initialGame() {
+  for (let i = 0; i < modeButton.length; i++) {
+    modeButton[i].addEventListener('click', function () {
+      modeButton[0].classList.remove('selected')
+      modeButton[1].classList.remove('selected')
+      this.classList.add('selected')
+      this.textContent === 'Easy' ? numberOfSquares = 3 : numberOfSquares = 6;
+      resetGame()
+    })
+  }
+  // check the clicked color
   for (let i = 0; i < squares.length; i++) {
-    squares[i].style.background = colors[i]
+    squares[i].addEventListener('click', function () {
+      let clickedColor = this.style.background;
+      if (clickedColor === pickedColor) {
+        messageDisplay.textContent = 'Correct'
+        changeAllSquaresColor(clickedColor)
+        h1.style.background = clickedColor
+        resetButton.textContent = 'Play Again?'
+      } else {
+        this.style.background = '#232323'
+        messageDisplay.textContent = 'Try It again'
+      }
+    })
   }
-  for (let i = 3; i < 6; i++) {
-    squares[i].classList.add('hide')
-  }
-})
-hardBtn.addEventListener('click', function () {
-  className(hardBtn, easyBtn)
-  numberOfSquares = 6
-  colors = randomColors(numberOfSquares)
-  pickedColor = pickColor()
-  h1.style.background = 'steelblue'
-  messageDisplay.textContent = ''
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].style.background = colors[i]
-  }
-  for (let i = 3; i < 6; i++) {
-    squares[i].classList.remove('hide')
-  }
-})
-//check the clicked color
-for (let i = 0; i < squares.length; i++) {
-  squares[i].style.background = colors[i]
-  squares[i].addEventListener('click', function () {
-    let clickedColor = this.style.background;
-    if (clickedColor === pickedColor) {
-      messageDisplay.textContent = 'Correct'
-      changeAllSquaresColor(clickedColor)
-      h1.style.background = clickedColor
-      resetButton.textContent = 'Play Again?'
-    } else {
-      this.style.background = '#232323'
-      messageDisplay.textContent = 'Try It again'
-    }
-  })
+  resetGame()
+
 }
+
+
+
+
+
+
+
+
+
 
 
 //change all squares' colors to the correct one
@@ -86,15 +80,15 @@ function resetGame() {
   colors = randomColors(numberOfSquares)
   pickedColor = pickColor();
   for (let i = 0; i < squares.length; i++) {
-    squares[i].style.background = colors[i]
+    if (colors[i]) {
+      squares[i].classList.remove('hide')
+      squares[i].style.background = colors[i]
+    } else {
+      squares[i].classList.add('hide')
+    }
   }
   colorDisplay.textContent = pickedColor;
   h1.style.background = 'steelblue';
   messageDisplay.textContent = '';
   resetButton.textContent = 'New Colors'
-}
-//add class name to a button
-function className(btn1, btn2) {
-  btn1.classList.add('selected')
-  btn2.classList.remove('selected')
 }
